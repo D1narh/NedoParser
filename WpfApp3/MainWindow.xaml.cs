2e1c763f064303ddb3aefbb24c98efb98d58ddc9
D1narh <dunis.guryanov@mail.ru>
2022-11-16 15:25:30 +0400

    <span class="image"> НАШЕЛСЯ Но нужны доработки

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Wordprocessing;
 using HtmlAgilityPack;
 using System;
@@ -30,11 +32,23 @@ namespace WpfApp3
             InitializeComponent();
 
             HtmlWeb web = new HtmlWeb();
-            HtmlDocument document = web.Load("https://101hotels.com/main/cities/moskva?viewType=tiles&page=2");
+            HtmlDocument document = web.Load("https://101hotels.com/main/cities/moskva?viewType=tiles&page=2");//проблема с отсрочкой в переменную
 
             var title2 = document.DocumentNode.SelectNodes("//*[@id=\"hidden-by-loader\"]").First().InnerText;
 
-            //var title = document.DocumentNode.SelectNodes("//*[@id=\"hidden-by-loader\"]/li[1]/article/div[1]/div[1]/div[1]/div/div[3]/span/img").First();
+            var title3 = document.DocumentNode.SelectNodes("//*[@itemprop=\"image\"]");
+
+
+            HtmlNodeCollection span = document.DocumentNode.SelectNodes("//span[@class=\"image\"]");
+
+            foreach(var item in span)
+            {
+
+                text2.Text += " " + item.InnerHtml.Replace("  ", " ");
+            }
+
+
+            //var title = document.DocumentNode.SelectNodes("//*[@id=\"hidden-by-loader\"]/li/article/div/div/div/div/div[3]/span/img").First();//проблема с изображением тут
 
             var texter = title2.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
                                .Where(str => !string.IsNullOrWhiteSpace(str))
@@ -51,6 +65,9 @@ namespace WpfApp3
                 stra = stra.Replace("  ", " ");
             }
             text.Text = stra.Trim();
+
+            //text2.Text = title3;
+
         }
     }
 }
